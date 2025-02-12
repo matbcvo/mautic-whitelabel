a984622c34f8caa871538d5b5c6b7db789e0a196
Martin Vooremäe <martin.vooremae@gmail.com>
2025-02-13 00:04:08 +0200

    fix

--- src/Command/WhitelabelCommand.php
+++ src/Command/WhitelabelCommand.php
@@ -4,6 +4,7 @@ namespace Matbcvo\MauticWhitelabel\Command;
 
 use Composer\Command\BaseCommand;
 use Composer\Factory as ComposerFactory;
+use RuntimeException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -12,6 +13,16 @@ use Symfony\Component\Dotenv\Exception\PathException;
 
 class WhitelabelCommand extends BaseCommand
 {
+    protected $input;
+    protected $output;
+
+    public function __construct(InputInterface $input, OutputInterface $output)
+    {
+        $this->input = $input;
+        $this->output = $output;
+        parent::__construct();
+    }
+
     protected function configure(): void
     {
         $this->setName('mautic:whitelabel')
@@ -49,45 +60,62 @@ class WhitelabelCommand extends BaseCommand
             return Command::SUCCESS;
         }
 
+        try {
+            $mauticSystemThemePath = $this->createSystemTheme($projectRootPath, $mauticWebRoot);
+            $this->copyLoginViewTemplate($projectRootPath, $mauticWebRoot, $mauticSystemThemePath);
+            $this->overrideLoginViewTemplate($mauticSystemThemePath);
+        } catch (\RuntimeException $e) {
+            $output->writeln(sprintf("<error>%s</error>", $e->getMessage()));
+            return Command::FAILURE;
+        }
+
+        return Command::SUCCESS;
+    }
+
+    private function createSystemTheme(string $projectRootPath, string $mauticWebRoot): string
+    {
         $mauticThemesPath = $projectRootPath.'/'.$mauticWebRoot.'themes';
         if (!is_dir($mauticThemesPath)) {
-            $output->writeln("<error>Mautic themes directory not found!</error>");
-            return Command::FAILURE;
+            throw new \RuntimeException("Mautic themes directory not found!");
         }
 
         $mauticSystemThemePath = $mauticThemesPath.'/system';
         if (!is_dir($mauticSystemThemePath)) {
-            $output->writeln("<info>Creating Mautic system theme directory</info>");
             mkdir($mauticSystemThemePath);
         }
 
+        return $mauticSystemThemePath;
+    }
+
+    private function copyLoginViewTemplate(string $projectRootPath, string $mauticWebRoot, string $mauticSystemThemePath): void
+    {
         // Login page
         // app/bundles/UserBundle/Resources/views/Security/base.html.twig
         $mauticLoginViewTemplatePath = $projectRootPath.'/'.$mauticWebRoot.'app/bundles/UserBundle/Resources/views/Security';
-        $output->writeln("mauticLoginViewTemplatePath: {$mauticLoginViewTemplatePath}");
+        $this->output->writeln("mauticLoginViewTemplatePath: {$mauticLoginViewTemplatePath}");
         $overrideLoginViewTemplatePath = $mauticSystemThemePath.'/UserBundle/Resources/views/Security';
-        $output->writeln("overrideLoginViewTemplatePath: {$overrideLoginViewTemplatePath}");
+        $this->output->writeln("overrideLoginViewTemplatePath: {$overrideLoginViewTemplatePath}");
 
         // Create directory for overriding view template
         mkdir($mauticSystemThemePath.'/UserBundle/Resources/views/Security', $permissions = 0777, $recursive = true);
 
         if (!is_dir($mauticSystemThemePath.'/UserBundle/Resources/views/Security')) {
-            $output->writeln("<error>Creating recursive login view template directory was not successful</error>");
-            return Command::FAILURE;
+            throw new \RuntimeException("Creating themes/system/UserBundle/Resources/views/Security directory was not successful");
         }
 
         // Copy view template file to override
-        copy($mauticLoginViewTemplatePath, $overrideLoginViewTemplatePath);
+        copy($mauticLoginViewTemplatePath.'/base.html.twig', $overrideLoginViewTemplatePath.'/base.html.twig');
+    }
 
-        $content = file_get_contents($overrideLoginViewTemplatePath);
+    private function overrideLoginViewTemplate(string $mauticSystemThemePath): void
+    {
+        $path = $mauticSystemThemePath.'/UserBundle/Resources/views/Security/base.html.twig';
+        $content = file_get_contents($path);
         $pattern = '/(<div[^>]*class\s*=\s*["\'][^"\']*\bmautic-logo\b[^"\']*["\'][^>]*>)(.*?)(<\/div>)/is';
         $replacement = '$1test$3';
         $newContent = preg_replace($pattern, $replacement, $content);
-        if (file_put_contents($overrideLoginViewTemplatePath, $newContent) === false) {
-            $output->writeln("<error>Error writing to the override login view template file</error>");
-            return Command::FAILURE;
+        if (file_put_contents($path, $newContent) === false) {
+            throw new \RuntimeException('Error writing to the override login view template file');
         }
-
-        return Command::SUCCESS;
     }
 }
